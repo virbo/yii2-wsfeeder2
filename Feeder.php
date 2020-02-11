d1e0d2adbc824f44933fd64455107ace668f3b85
Yusuf Ayuba <yusufayuba@live.com>
2020-02-11 09:33:05 +0800

    update lagi (belum fixed)

--- Feeder.php
+++ Feeder.php
@@ -2,9 +2,12 @@
 
 namespace virbo\wsfeeder2;
 
+use Yii;
 use yii\base\Component;
+use yii\helpers\ArrayHelper;
 use yii\httpclient\client;
 use yii\web\BadRequestHttpException;
+use yii\helpers\Json;
 
 /**
  * This is just an example.
@@ -48,46 +51,73 @@ class Feeder extends Component
         ]);
 
         $this->getToken();
-
-        return $this->_client;
     }
 
-    public function getToken()
-    {
-        $act = json_encode([
-            'act' => 'GetToken',
-            'username' => $this->username,
-            'password' => $this->password
-        ]);
-
-        $responseWs = $this->_client->createRequest()
-            ->setUrl($this->mode)
-            ->addHeaders(['content-type' => 'application/json'])
-            ->setContent($act)
-            ->setMethod('post')
-            ->send();
-
-        if ($responseWs->data['error_code'] == 0) {
-            $this->_token = $responseWs->data['data']['token'];
-        } else {
-            throw new BadRequestHttpException();
-        }
-
-        return $this->_token;
-    }
-
-    public function actFeeder($data)
+    public function actFeeder($data = [])
     {
         $mode = $this->mode == 0 ? 'live2.php' : 'sandbox2.php';
+
+        $token = ['token' => $this->_token];
+
+        $data = ArrayHelper::merge($data, $token);
 
         $request = $this->_client->createRequest()
             ->setUrl('/'.$mode)
             ->addHeaders(['content-type' => 'application/json'])
-            ->setContent($data)
+            ->setContent(Json::encode($data))
             ->setMethod('post')
             ->send();
 
-        return $request;
+        if ($request) {
+            if ($request->data['error_code'] == 0) {
+                return $request;
+            } else {
+                if ($request->data['error_code'] === 100) {
+                    $session = Yii::$app->session;
+                    $session['token'] == null;
+                } else {
+                    throw new BadRequestHttpException('Error '.$request->data['error_code'].' - '.$request->data['error_desc']);
+                }
+            }
+        } else {
+            throw new BadRequestHttpException();
+        }
+    }
+
+    protected function getToken()
+    {
+        $session = Yii::$app->session;
+
+        if ($session['token'] === null) {
+            $act = Json::encode([
+                'act' => 'GetToken',
+                'username' => $this->username,
+                'password' => $this->password
+            ]);
+
+            $mode = $this->mode == 0 ? 'live2.php' : 'sandbox2.php';
+
+            $request = $this->_client->createRequest()
+                ->setUrl($mode)
+                ->addHeaders(['content-type' => 'application/json'])
+                ->setContent($act)
+                ->setMethod('post')
+                ->send();
+
+            if ($request) {
+                if ($request->data['error_code'] == 0) {
+                    $session['token'] = $request->data['data']['token'];
+                } else {
+                    throw new BadRequestHttpException('Error '.$request->data['error_code'].' - '.$request->data['error_desc']);
+                }
+            } else {
+                throw new BadRequestHttpException();
+            }
+
+            return $this->_token = $session['token'];
+        } else {
+            return $this->_token = $session['token'];
+        }
     }
 
 }
